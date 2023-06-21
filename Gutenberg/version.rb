dec735b7c6cb4a2fe51205003c9733909bb87937
Gio Lodi <gio@mokacoding.com>
2023-06-21 21:47:15 +1000

    Switch to Gutenberg v1.97.1 installed as an XCFramework

--- Gutenberg/version.rb
+++ Gutenberg/version.rb
@@ -11,8 +11,13 @@
 #
 #   LOCAL_GUTENBERG=../my-gutenberg-fork bundle exec pod install
 GUTENBERG_CONFIG = {
-  # commit: '',
-  tag: 'v1.97.1'
+  # To facilitate the adoption of the XCFramework setup, which greatly speeds up `pod install`,
+  # we shipped an ad hoc commit build with the v1.97.1 code,
+  # even though the codebase for that tag did not have the necessary automation.
+  # This approach will no longer be necessary starting v1.98.0,
+  # which should be cut from `trunk` at a point where all the necessary automation is already in place.
+  commit: 'ce86d9eafeb6b83c38eb7f4745d4b9be96675778'
+  # tag: 'v1.97.1'
 }
 
 GITHUB_ORG = 'wordpress-mobile'
